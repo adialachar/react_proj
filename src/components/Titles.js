dc750ed8a1bf1b46c9a99ab23ced63f59a662300
root <root@DESKTOP-BVUBJUM.localdomain>
2019-07-01 21:29:13 -0700

    initial commit

---
+++ src/components/Titles.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+class Titles extends React.Component {
+
+
+	render(){
+
+		return(
+
+			<div>
+				<h1> weather finder </h1>
+				<p> find out temperature conditions and more </p>
+			</div>
+
+		);
+
+	}
+};
+
+export default Titles;
